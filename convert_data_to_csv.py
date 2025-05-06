c33b4350ad8e1d0c4609ee4dd2d527eb57ab6b1b
rkozak <rkozak@expedia.com>
2025-05-06 23:58:39 +0200

    .

---
+++ convert_data_to_csv.py
@@ -0,0 +1,12 @@
+import tsplib95
+import csv
+
+problem = tsplib95.load("data/pliku.tsp")
+
+with open("miasta.csv", mode="w", newline="") as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(["CityID", "X", "Y"])  # nagłówki
+
+    for city_id, (x, y) in problem.node_coords.items():
+        writer.writerow([city_id, x, y])
+print('abc')
